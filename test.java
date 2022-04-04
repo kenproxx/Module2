f149638714a9d3d6ac037f6121c803f749b22d95
kenproxx <kienhue98@gmail.com>
2022-04-04 11:52:16 +0700

    hihi

---
+++ test.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        String[] arr = {
+                "khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin", "muoi"
+        };
+        int hi = num % 10;
+        int a = num / 10;
+
+        if(hi == 0 ){
+            System.out.println(arr[a] + " muoi");
+            return;
+        }
+        if(10 < num && num <= 20 ) {
+            System.out.println(" muoi " + arr[hi]);
+            return;
+        }
+        if(20 < num && num < 99){
+            System.out.println(arr[a] + " muoi " + arr[hi]);
+            return;
+        }
+        System.out.println(arr[num]);
+
+        }
+}
